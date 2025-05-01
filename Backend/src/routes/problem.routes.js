be991c892fa94cdcef0c901c5de9b9699cb3ba20
GarvitRakhechaa <garvitrakhecha786@gmail.com>
2025-05-01 10:29:32 +0530

    created check admin middleware and initiated problem controller

--- Backend/src/routes/problem.routes.js
+++ Backend/src/routes/problem.routes.js
@@ -1,6 +1,9 @@
 import express from "express"
+import { authMiddleware, checkAdmin } from "../middleware/auth.middleware.js"
+import { createProblem } from "../constrollers/problem.controller.js"
 
 const problemRoutes = express.Router()
 
+problemRoutes.post("/create-problem", authMiddleware, checkAdmin, createProblem)
 
 export default problemRoutes
